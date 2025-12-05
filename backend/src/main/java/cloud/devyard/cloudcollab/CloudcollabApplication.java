c85b52654f2e7b1a7578d040e9bf3f15641e64c2
aditya mehta <adityamehta242@gmail.com>
2025-12-06 01:21:54 +0530

    backend setup

---
+++ backend/src/main/java/cloud/devyard/cloudcollab/CloudcollabApplication.java
@@ -0,0 +1,13 @@
+package cloud.devyard.cloudcollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudcollabApplication.class, args);
+	}
+
+}
