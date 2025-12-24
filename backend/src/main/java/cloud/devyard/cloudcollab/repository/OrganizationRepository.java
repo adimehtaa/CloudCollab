b3e499fbfff42c866979b2f1bd85a45687d603ed
aditya mehta <adityamehta242@gmail.com>
2025-12-24 22:35:23 +0530

    add Repository class

--- backend/src/main/java/cloud/devyard/cloudcollab/repository/OrganizationRepository.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/repository/OrganizationRepository.java
@@ -1,4 +1,13 @@
 package cloud.devyard.cloudcollab.repository;
 
-public class OrganizationRepository {
+import cloud.devyard.cloudcollab.model.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrganizationRepository extends JpaRepository<Organization , Long> {
+    Optional<Organization> findBySlug(String slug);
+    Boolean existsBySlug(String slug);
 }
--- backend/src/main/java/cloud/devyard/cloudcollab/repository/RefreshTokenRepository.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/repository/RefreshTokenRepository.java
@@ -1,4 +1,14 @@
 package cloud.devyard.cloudcollab.repository;
 
-public class RefreshTokenRepository {
+import cloud.devyard.cloudcollab.model.RefreshToken;
+import cloud.devyard.cloudcollab.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    void deleteByUser(User user);
 }
