a70c5a644040110d3171d40eeefaa4676c62dd58
aditya mehta <adityamehta242@gmail.com>
2026-01-23 23:54:35 +0530

    add invite user

--- backend/src/main/java/cloud/devyard/cloudcollab/service/UserInvitationService.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/service/UserInvitationService.java
@@ -1,5 +1,10 @@
 package cloud.devyard.cloudcollab.service;
 
+import cloud.devyard.cloudcollab.dto.request.InviteUserRequest;
+import cloud.devyard.cloudcollab.dto.response.InvitationResponse;
+
 public interface UserInvitationService {
+
+    public InvitationResponse inviteUser(InviteUserRequest request, Long organizationId, Long invitedByUserId);
 
 }
