c3d1539daa5add64259d7760224a0af23bd1904a
aditya mehta <adityamehta242@gmail.com>
2026-02-09 00:05:09 +0530

    add FilePermission

--- backend/src/main/java/cloud/devyard/cloudcollab/model/FilePermission.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/model/FilePermission.java
@@ -1,5 +1,22 @@
 package cloud.devyard.cloudcollab.model;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "file_permissions")
 public class FilePermission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
