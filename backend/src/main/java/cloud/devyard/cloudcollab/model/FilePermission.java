033c6cbac27425b021f70af1d00bab72ba654b30
aditya mehta <adityamehta242@gmail.com>
2026-02-10 00:55:22 +0530

    add FilePermission and FileRepository

--- backend/src/main/java/cloud/devyard/cloudcollab/model/FilePermission.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/model/FilePermission.java
@@ -1,22 +1,49 @@
 package cloud.devyard.cloudcollab.model;
 
 
+import cloud.devyard.cloudcollab.model.enums.PermissionType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "file_permissions")
+@Table(name = "file_permissions",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"file_id", "user_id"}))
 public class FilePermission {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "file_id", nullable = false)
+    private File file;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PermissionType permissionType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "granted_by_user_id")
+    private User grantedBy;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime grantedAt;
+
+    private LocalDateTime expiresAt;
 
 }
--- backend/src/main/java/cloud/devyard/cloudcollab/repository/FileRepository.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/repository/FileRepository.java
@@ -1,4 +1,15 @@
 package cloud.devyard.cloudcollab.repository;
 
-public interface FileRepository {
+import cloud.devyard.cloudcollab.model.File;
+import cloud.devyard.cloudcollab.model.enums.FileStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<File , Long> {
+    Page<File> findByOrganizationIdAndStatusAndFolderIsNull(
+            Long organizationId, FileStatus status, Pageable pageable
+    );
 }
