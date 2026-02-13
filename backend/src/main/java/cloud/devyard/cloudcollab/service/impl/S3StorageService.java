9de04191789381add2878c2edc7d82831267644a
aditya mehta <adityamehta242@gmail.com>
2026-02-14 01:14:14 +0530

    service implementation remain and properties is config done

--- backend/src/main/java/cloud/devyard/cloudcollab/service/impl/S3StorageService.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/service/impl/S3StorageService.java
@@ -1,11 +1,30 @@
 package cloud.devyard.cloudcollab.service.impl;
 
 import cloud.devyard.cloudcollab.service.StorageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 import java.io.InputStream;
 
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class S3StorageService implements StorageService {
+
+    private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    @Value("${aws.cloudfront.domain}")
+    private String cloudfrontDomain;
+
     @Override
     public String uploadFile(MultipartFile file, String folder, Long organizationId) {
         return "";
