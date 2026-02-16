10e59b8bc371a17c91e0e8f387915dbda97be807
aditya mehta <adityamehta242@gmail.com>
2026-02-17 00:50:16 +0530

    Add Folder repository

--- backend/src/main/java/cloud/devyard/cloudcollab/repository/FolderRepository.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/repository/FolderRepository.java
@@ -1,4 +1,9 @@
 package cloud.devyard.cloudcollab.repository;
 
-public class FolderRepository {
+import cloud.devyard.cloudcollab.model.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder , Long > {
 }
