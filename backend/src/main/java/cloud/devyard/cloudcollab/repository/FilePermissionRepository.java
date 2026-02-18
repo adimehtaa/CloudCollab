fc3a316292aed5f14c2ba218d5156d5f94dd9d77
aditya mehta <adityamehta242@gmail.com>
2026-02-19 00:05:50 +0530

    add FilePermissionRepository

--- backend/src/main/java/cloud/devyard/cloudcollab/repository/FilePermissionRepository.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/repository/FilePermissionRepository.java
@@ -1,4 +1,21 @@
 package cloud.devyard.cloudcollab.repository;
 
-public interface FilePermissionRepository {
+import cloud.devyard.cloudcollab.model.FilePermission;
+import cloud.devyard.cloudcollab.model.enums.PermissionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FilePermissionRepository extends JpaRepository<FilePermission, Long> {
+
+    List<FilePermission> findByFileId(Long fileId);
+
+    Optional<FilePermission> findByFileIdAndUserId(Long fileId, Long userId);
+
+    List<FilePermission> findByUserIdAndPermissionType(Long userId, PermissionType permissionType);
+
+    void deleteByFileIdAndUserId(Long fileId, Long userId);
 }
