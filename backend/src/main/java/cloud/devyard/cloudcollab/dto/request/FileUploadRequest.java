04b74e59436f8900c149b8046dc04d634651e2d5
aditya mehta <adityamehta242@gmail.com>
2026-02-28 23:50:07 +0530

    add dto for fileupload

--- backend/src/main/java/cloud/devyard/cloudcollab/dto/request/FileUploadRequest.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/dto/request/FileUploadRequest.java
@@ -1,4 +1,16 @@
 package cloud.devyard.cloudcollab.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class FileUploadRequest {
+    private String description;
+    private Long folderId;
+    private Boolean isPublic;
 }
