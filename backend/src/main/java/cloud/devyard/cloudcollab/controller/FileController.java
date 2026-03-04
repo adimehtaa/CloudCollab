9df45b6426ab2d9383c2cbe31e2bb5d01753ad2d
aditya mehta <adityamehta242@gmail.com>
2026-03-04 23:58:53 +0530

    add controller

--- backend/src/main/java/cloud/devyard/cloudcollab/controller/FileController.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/controller/FileController.java
@@ -1,4 +1,9 @@
 package cloud.devyard.cloudcollab.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/files")
 public class FileController {
 }
