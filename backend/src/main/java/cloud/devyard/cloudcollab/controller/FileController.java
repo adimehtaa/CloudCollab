547df82d790aa2e86b4b3d101657a523f3f9c19f
aditya mehta <adityamehta242@gmail.com>
2026-03-18 23:53:38 +0530

    add controller

--- backend/src/main/java/cloud/devyard/cloudcollab/controller/FileController.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/controller/FileController.java
@@ -1,9 +1,63 @@
 package cloud.devyard.cloudcollab.controller;
 
+import cloud.devyard.cloudcollab.dto.ApiResponse;
+import cloud.devyard.cloudcollab.dto.request.FileUploadRequest;
+import cloud.devyard.cloudcollab.dto.response.FileResponse;
+import cloud.devyard.cloudcollab.dto.response.Status;
+import cloud.devyard.cloudcollab.security.UserPrincipal;
+import cloud.devyard.cloudcollab.service.FileService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/files")
+@RequiredArgsConstructor
 public class FileController {
+
+    private final FileService fileService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<@NonNull ApiResponse<FileResponse>> uploadFile(
+            @AuthenticationPrincipal UserPrincipal currentUser,
+            @RequestParam("file") MultipartFile file,
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) Long folderId,
+            @RequestParam(required = false) Boolean isPublic,
+            HttpServletRequest httpRequest
+    ){
+
+        FileUploadRequest request = FileUploadRequest.builder()
+                .description(description)
+                .folderId(folderId)
+                .isPublic(isPublic)
+                .build();
+
+        FileResponse fileResponse = fileService.uploadFile(
+                file,
+                request,
+                currentUser.getId(),
+                currentUser.getOrganizationId(),
+                httpRequest);
+
+        var response = ApiResponse.<FileResponse>builder()
+                .status(Status.SUCCESS)
+                .statusCode(HttpStatus.OK.value())
+                .message("Upload file successfully.")
+                .data(fileResponse)
+                .build();
+
+        return ResponseEntity.ok(response);
+
+    }
 }
