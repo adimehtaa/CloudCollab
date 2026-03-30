f982547f62070641e8c0ca02ca5a27de0ac95ad3
aditya mehta <adityamehta242@gmail.com>
2026-03-30 23:55:20 +0530

    add createFolder

--- backend/src/main/java/cloud/devyard/cloudcollab/controller/FolderController.java
+++ backend/src/main/java/cloud/devyard/cloudcollab/controller/FolderController.java
@@ -1,9 +1,42 @@
 package cloud.devyard.cloudcollab.controller;
 
+import cloud.devyard.cloudcollab.dto.ApiResponse;
+import cloud.devyard.cloudcollab.dto.request.CreateFolderRequest;
+import cloud.devyard.cloudcollab.dto.response.FolderResponse;
+import cloud.devyard.cloudcollab.dto.response.Status;
+import cloud.devyard.cloudcollab.security.UserPrincipal;
+import cloud.devyard.cloudcollab.service.FolderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/folders")
+@RequiredArgsConstructor
 public class FolderController {
+    private final FolderService folderService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<FolderResponse>> createFolder(
+            @AuthenticationPrincipal UserPrincipal currentUser,
+            @Valid @RequestBody CreateFolderRequest request
+    ){
+
+        FolderResponse folder = folderService.createFolder(request, currentUser.getId(), currentUser.getOrganizationId());
+        var response = ApiResponse.<FolderResponse>builder()
+                .status(Status.SUCCESS)
+                .statusCode(HttpStatus.CREATED.value())
+                .data(folder)
+                .message("Folder successfully created.")
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
 }
